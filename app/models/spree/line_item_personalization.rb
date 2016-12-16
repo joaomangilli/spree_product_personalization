d29ac5ae38bf38e1c55b469ed14f33deaa63048c
Karl Smith <kesmith@godaddy.com>
2016-12-16 15:05:17 -0700

    add relation back to option value product personalization, fix a refactor error

--- app/models/spree/line_item_personalization.rb
+++ app/models/spree/line_item_personalization.rb
@@ -2,6 +2,7 @@ module Spree
   class LineItemPersonalization < ActiveRecord::Base
     belongs_to :line_item
     belongs_to :product_personalization, class_name: "Spree::ProductPersonalization", foreign_key: "spree_product_personalization_id"
+    belongs_to :option_value_product_personalization, class_name: "Spree::OptionValueProductPersonalization", foreign_key: "spree_option_value_product_personalization_id"
 
     validate :value_length
 
@@ -24,18 +25,22 @@ module Spree
       @option_value_id
     end
 
-    def option_value_id=(id)
-      self.value = Spree::OptionValue.find_by(id: id).name
-      @option_value_id = id
+    def option_value_id=(value)
+      option_value = Spree::OptionValue.find_by(id: value)
+      self.value = option_value.name
+      @option_value_id = value
+
+      ovpp = option_value.option_value_product_personalizations.find_by(option_value_id: value)
+      self.spree_option_value_product_personalization_id = ovpp.try(:id)
     end
 
     private
 
     def value_length
       if value.size < 1
-        errors.add(:base, { name => Spree.t('errors.line_item_personalization_value_is_required', name: name) })
+        errors.add(:base, {name => Spree.t('errors.line_item_personalization_value_is_required', name: name) })
       elsif value.size > limit
-        errors.add(:base, { name => Spree.t('errors.line_item_personalization_value_is_too_long', name: name, size: limit) })
+        errors.add(:base, {name => Spree.t('errors.line_item_personalization_value_is_too_long', name: name, size: limit) })
       end
     end
   end
